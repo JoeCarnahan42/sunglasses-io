7924aae7eeb160442cd19323ff138699023ccdcb
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-02-24 16:37:14 -0600

    Start writing test

--- app/server.js
+++ app/server.js
@@ -1,31 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
 
 app.use(bodyParser.json());
 
 // Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+const users = require("../initial-data/users.json");
+let brands = require("../initial-data/brands.json");
+const products = require("../initial-data/products.json");
+
+// Local Data
+const cart = [];
 
 // Error handling
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Starting the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/brands", (req, res) => {
+  if (brands.length < 1) {
+    res.status(400).end("No brands found.");
+  } else {
+    res.status(200).json(brands);
+  }
+});
+
+app.get("/products", (req, res) => {
+  res.status(200).json(products);
+});
+
+app.get("/me/cart", (req, res) => {
+  res.status(200).json(cart);
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,97 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app/server"); // Adjust the path as needed
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
+// Routes to support:
+// GET: brands(DONE), brands/:id/products, products(DONE), me/cart(DONE)
+// POST: login, me/cart, me/cart/:productId
+// DELETE: me/cart/:productId
 
-describe('Brands', () => {});
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("should return an error if there are no brands", (done) => {
+      chai.end((err, res) => {
+        res.body = "No brands found.";
+        res.should.have.status(400);
+        res.body.should.be.an("string");
+        res.body.should.eql("No brands found.");
+        done();
+      });
+    });
+    it("should GET all brands", (done) => {
+      chai
+        .request(server)
+        .get("/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+  describe("/GET products", () => {
+    it("should GET all products", (done) => {
+      chai
+        .request(server)
+        .get("/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+  describe("/GET brands/:id/products", () => {
+    it("should GET all products of specified brand", (done) => {
+      chai
+        .request(server)
+        .get("/brands/:id/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          // Finish Test
+        });
+    });
+  });
+});
 
-describe('Login', () => {});
+describe("Login", () => {
+  describe("/POST login", () => {
+    it("should login successfully with correct credentials", (done) => {
+      // Write Test
+    });
+  });
+});
 
-describe('Cart', () => {});
+describe("Cart", () => {
+  describe("/GET me/cart", () => {
+    it("should return all items in the cart", (done) => {
+      chai
+        .request(server)
+        .get("/me/cart")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+  });
+
+  describe("/POST me/cart", () => {
+    //What should this do?
+  });
+
+  describe("/POST me/card/:productId", () => {
+    it("should POST a specifit item to the cart", (done) => {});
+  });
+
+  describe("/DELETE me/cart/:productId", () => {
+    it("should DELETE a specific item from the cart", (done) => {});
+  });
+});
