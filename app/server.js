4a16b9a28b22bc4879e3aa699ef571b5ca208e24
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-02-25 09:33:51 -0600

    complete some tests and routes

--- app/server.js
+++ app/server.js
@@ -5,12 +5,13 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
+require("dotenv").config();
 
 app.use(bodyParser.json());
 
 // Importing the data from JSON files
 const users = require("../initial-data/users.json");
-let brands = require("../initial-data/brands.json");
+const brands = require("../initial-data/brands.json");
 const products = require("../initial-data/products.json");
 
 // Local Data
@@ -26,17 +27,24 @@ app.use((err, req, res, next) => {
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Starting the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+// Brand routes
 app.get("/brands", (req, res) => {
-  if (brands.length < 1) {
-    res.status(400).end("No brands found.");
-  } else {
-    res.status(200).json(brands);
-  }
+  res.status(200).json(brands);
+});
+
+app.get("/brands/:id/products", (req, res) => {
+  // return an array of products from a particular brand
+  const brandId = String(req.params.id);
+  const filterProducts = products.filter((product) => {
+    const categoryId = String(product.categoryId);
+    return categoryId === brandId;
+  });
+  res.status(200).json(filterProducts);
 });
 
 app.get("/products", (req, res) => {
--- test/server.test.js
+++ test/server.test.js
@@ -7,22 +7,13 @@ chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 // Routes to support:
-// GET: brands(DONE), brands/:id/products, products(DONE), me/cart(DONE)
+// GET: brands(DONE), brands/:id/products(DONE), products(DONE), me/cart
 // POST: login, me/cart, me/cart/:productId
 // DELETE: me/cart/:productId
 
 describe("Brands", () => {
   describe("/GET brands", () => {
-    it("should return an error if there are no brands", (done) => {
-      chai.end((err, res) => {
-        res.body = "No brands found.";
-        res.should.have.status(400);
-        res.body.should.be.an("string");
-        res.body.should.eql("No brands found.");
-        done();
-      });
-    });
-    it("should GET all brands", (done) => {
+    it("should return all brands", (done) => {
       chai
         .request(server)
         .get("/brands")
@@ -42,7 +33,6 @@ describe("Brands", () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          res.body.length.should.be.greaterThan(0);
           done();
         });
     });
@@ -51,30 +41,7 @@ describe("Brands", () => {
     it("should GET all products of specified brand", (done) => {
       chai
         .request(server)
-        .get("/brands/:id/products")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an("array");
-          // Finish Test
-        });
-    });
-  });
-});
-
-describe("Login", () => {
-  describe("/POST login", () => {
-    it("should login successfully with correct credentials", (done) => {
-      // Write Test
-    });
-  });
-});
-
-describe("Cart", () => {
-  describe("/GET me/cart", () => {
-    it("should return all items in the cart", (done) => {
-      chai
-        .request(server)
-        .get("/me/cart")
+        .get("/brands/1/products")
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
@@ -82,16 +49,28 @@ describe("Cart", () => {
         });
     });
   });
-
-  describe("/POST me/cart", () => {
-    //What should this do?
-  });
-
-  describe("/POST me/card/:productId", () => {
-    it("should POST a specifit item to the cart", (done) => {});
-  });
-
-  describe("/DELETE me/cart/:productId", () => {
-    it("should DELETE a specific item from the cart", (done) => {});
-  });
 });
+
+// describe("Login", () => {
+//   describe("/POST login", () => {
+//     it("should login successfully with correct credentials", (done) => {
+//       // Write Test
+//     });
+//   });
+// });
+
+// describe("Cart", () => {
+//   describe("/GET me/cart", () => {
+//     it("should return all items in the cart if a user is logged in", (done) => {});
+//   });
+
+//   describe("/POST me/cart", () => {});
+
+//   describe("/POST me/cart/:productId", () => {
+//     it("should POST a specifit item to the cart", (done) => {});
+//   });
+
+//   describe("/DELETE me/cart/:productId", () => {
+//     it("should DELETE a specific item from the cart", (done) => {});
+//   });
+// });
